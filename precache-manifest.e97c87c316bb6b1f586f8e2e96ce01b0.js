dba3855f2e76d0036ce78527fd59345c0ed6a8b2
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2019-01-08 20:57:55 +0900

    Updates

---
+++ precache-manifest.e97c87c316bb6b1f586f8e2e96ce01b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e1ca847d4ecbc025780",
+    "url": "/interest/static/js/runtime~main.8e1ca847.js"
+  },
+  {
+    "revision": "c374fde29fafb61f07a3",
+    "url": "/interest/static/js/main.c374fde2.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/interest/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "c374fde29fafb61f07a3",
+    "url": "/interest/static/css/main.4141bf09.chunk.css"
+  },
+  {
+    "revision": "1257436264f3c363df383714078374a2",
+    "url": "/interest/index.html"
+  }
+];
