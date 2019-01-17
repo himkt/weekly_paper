4cc9e3eb4d8657cfd06e99be79b73e52240ae0b5
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2019-01-17 17:18:50 +0900

    Updates

---
+++ precache-manifest.3b159a7a3b23e9823c4f83d6f6804e10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e1ca847d4ecbc025780",
+    "url": "/interest/static/js/runtime~main.8e1ca847.js"
+  },
+  {
+    "revision": "21c1cfefcae78c281c83",
+    "url": "/interest/static/js/main.21c1cfef.chunk.js"
+  },
+  {
+    "revision": "369a4d2cbe3a70dca16a",
+    "url": "/interest/static/js/1.369a4d2c.chunk.js"
+  },
+  {
+    "revision": "21c1cfefcae78c281c83",
+    "url": "/interest/static/css/main.eeb0310f.chunk.css"
+  },
+  {
+    "revision": "e353b5450098abda27db6e083afdc752",
+    "url": "/interest/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/interest/precache-manifest.5d72375f9f5eaca20950c0c181853772.js"
+  "/interest/precache-manifest.3b159a7a3b23e9823c4f83d6f6804e10.js"
 );
 
 workbox.clientsClaim();
